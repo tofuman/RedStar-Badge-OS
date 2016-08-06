8b5897bc591113cef64ad0851126bb4773efcfaf
Michel von Czettritz <michel@vczettritz.de>
2016-08-06 16:17:37 +0100

    make backround blinken

--- apps/sponsors/main.py
+++ apps/sponsors/main.py
@@ -7,6 +7,7 @@ import ugfx, pyb, buttons
 from imu import IMU
 import onboard
 import wifi
+import random
 
 def batteryled():
     ledg = pyb.LED(2)
@@ -37,25 +38,34 @@ else:
 	ugfx.orientation(180)
 
 def screen_1():
-    ugfx.display_image(0, 0, "apps/sponsors/splash1.gif")
+    ugfx.display_image(0, 0, "apps/sponsors/splash3.gif")
 
 def screen_2():
+    color = random.randint(0, 0xffffff)
+    ugfx.clear(ugfx.html_color(color))
     ugfx.display_image(0, 0, "apps/sponsors/sponsors2.gif")
 
 def screen_3():
     ugfx.clear(ugfx.html_color(0x7c1143))
     ugfx.text(27, 90, "@tofu_li", ugfx.WHITE)
 
-SCREENS = [screen_2, screen_1, screen_3]
+SCREENS = [screen_1, screen_3]
 SCREEN_DURATION = 2000
 
+
 screen_index = -1
-next_change = 0;
+next_change = 0
+next_color_change = 0
 while True:
     if pyb.millis() > next_change:
         screen_index = (screen_index + 1) % len(SCREENS)
         SCREENS[screen_index]()
         next_change = pyb.millis() + SCREEN_DURATION
+        next_color_change = pyb.millis()  + (SCREEN_DURATION/10)
+    elif pyb.millis() > next_color_change:
+        SCREENS[screen_index]()
+        next_color_change = pyb.millis()  + (SCREEN_DURATION/10)
+
     pyb.wfi()
     if buttons.is_triggered("BTN_MENU"):
         break;
